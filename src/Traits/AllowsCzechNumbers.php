eb6d32886c289206fb102768a7ebf8ad8f7facd6
Martin Kryl <martin@baoweb.cz>
2021-10-25 17:39:52 +0200

    Adding a trait and a validation rule for czech numeric notation

---
+++ src/Traits/AllowsCzechNumbers.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Baoweb\Helpers\Traits;
+
+trait AllowsCzechNumbers
+{
+    protected function getCzechNumericFields(): array
+    {
+        if (property_exists($this, 'czech_numeric_fields')) {
+            return $this->czech_numeric_fields;
+        }
+
+        return [];
+    }
+
+    public function setAttribute($key, $value)
+    {
+        if (in_array($key, $this->getCzechNumericFields(), true)) {
+            $value = str_replace(',', '.', $value);
+        }
+
+        return parent::setAttribute($key, $value);
+    }
+
+}
