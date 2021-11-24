5c70d8d186d0ef80eae2b3cba46169fd222a2f61
Martin Kryl <martin@baoweb.cz>
2021-11-24 14:13:04 +0100

    helpers

--- src/Helpers.php
+++ src/Helpers.php
@@ -5,4 +5,10 @@ namespace Baoweb\Helpers;
 class Helpers
 {
     // Build wonderful things
+
+    public function formatNumber($number, $decimals = 2) {
+        $formattedNumber = number_format($number, $decimals, ',', ' ');
+
+        return str_replace(' ', '&nbsp;', $formattedNumber);
+    }
 }
